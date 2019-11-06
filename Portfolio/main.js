60d95860f76c71d2d3f80f6c6a9534fc6d6e299e
Asha <ashalbenjamin@gmail.com>
2019-11-06 16:59:25 -0600

    third-commit-must-fix-css

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -4,3 +4,25 @@ $(document).ready(function() {
 		$('.top-nav').toggleClass('open');
 	});
 });
+
+$('.top-nav .nav-link').on('click', function() {
+	$('.menu-toggler').removeClass('open');
+	$('.top-nav').removeClass('open');
+});
+
+$('nav a[href*="#"]').on('click', function() {
+    $('html, body').animate(keyframes:{
+        scrolTop: $($(this).attr('href')).offset().top -100
+    }, options:2000)
+})
+
+$('#up').on('click', function() {
+    $('html, body').animate(keyframes:{
+        scrolTop: 0
+    }, options:2000)
+});
+
+Aos.init({
+    easing: 'ease',
+    duration: 1800
+});
